84b334488f9aba3d67c3a7470576e4387cba36ec
MEECHAI Vincent <43035257+PokSoul@users.noreply.github.com>
2021-11-19 11:21:57 +0100

    page favoris front only

--- src/components/topNavbar/index.js
+++ src/components/topNavbar/index.js
@@ -22,7 +22,9 @@ const TopNavbar = () => {
     <ContainerTop>
       <Name>{process.env.REACT_APP_NAME}</Name>
       <GamePage>{t('ListOfGames')}</GamePage>
-      <FavPage>{t('ListOfFavoris')}</FavPage>
+      <FavPage onClick={() => history.push('/favoris')}>
+        {t('ListOfFavoris')}
+      </FavPage>
       <Log onClick={() => history.push('/login')}>{t('Login')}</Log>
       <ButtonTheme
         onClick={() =>
---
+++ src/screens/favorites.js
@@ -0,0 +1,79 @@
+import React, { useEffect } from 'react'
+import styled from 'styled-components'
+import { useSelector, useDispatch } from 'react-redux'
+import allTheActions from '../actions'
+import GameDisplay from '../components/gameDisplay'
+import { DeleteBin } from '@styled-icons/remix-line/DeleteBin'
+import Navbar from './navbar'
+
+const FavoritesList = () => {
+  const dispatch = useDispatch()
+  const listOfGames = useSelector(state => state.gamesApi.value)
+
+  useEffect(() => {
+    dispatch(
+      allTheActions.gamesApi.callApiGames(
+        'https://store.steampowered.com/api/featured/'
+      )
+    )
+  }, [])
+
+  return (
+    <div>
+      <Navbar />
+      <Container>
+        <h1>Favorites</h1>
+        <GamesContainer>
+          {listOfGames?.data?.featured_win.map(game => {
+            return (
+              <div key={game.id}>
+                <StyledBin />
+                <GameDisplay
+                  image={game.large_capsule_image}
+                  title={game.name}
+                  price={`${game.final_price} ${game.currency}`}
+                  tag={'test'}
+                  id={game.id}
+                  suggestion={true}
+                />
+                <br />
+              </div>
+            )
+          })}
+        </GamesContainer>
+      </Container>
+    </div>
+  )
+}
+
+const getDisplay = () => {
+  const display = useSelector(state => state.display.value)
+  return display.width < 1024 ? '5%' : '2%'
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const GamesContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: center;
+  margin-left: ${getDisplay};
+  margin-top: ${getDisplay};
+`
+const GamesContainerChild = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+const StyledBin = styled(DeleteBin)`
+  max-width: 50px;
+  color: red;
+`
+
+export default FavoritesList
